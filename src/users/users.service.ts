911cd4edc44953a36af1bc7df7315736d24d7231
Bosco <boscomontalvao@hotmail.com.br>
2025-07-13 22:47:28 -0300

    inicio com todas as configs

---
+++ src/users/users.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from './../prisma/prisma.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createUserDto: CreateUserDto) {
+    return this.prismaService.user.create({
+      data: createUserDto,
+    });
+  }
+
+  findAll() {
+    return this.prismaService.user.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return this.prismaService.user.update({
+      where: {
+        id,
+      },
+      data: updateUserDto,
+    });
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
