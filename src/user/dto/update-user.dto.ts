759705f105f176ceab13aafecb9e75cef86882e4
Bosco <boscomontalvao@hotmail.com.br>
2025-08-04 18:23:07 -0300

    other test any

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/swagger';
+
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  a: any;
+  b: any
+  c: any;
+  d: any
+  f: any;
+  e: any;
+  g: any
+}
