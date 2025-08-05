8a556f72aa583b35be04fd7c045342a9d1021ecb
Bosco <boscomontalvao@hotmail.com.br>
2025-08-05 14:22:27 -0300

    config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,6 @@ import hiporepo from 'eslint-config-hiporepo';
 export default [
   ...hiporepo,
   {
-    rules: {
-      // Desabilita a regra no-explicit-any para o SonarQube
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-  },
-  {
     ignores: ['**/*.spec.ts', '**/*.test.ts', 'test/', '__tests__/',
       '.vscode', 'node_modules', 'public', 'generated', 'dist/'
     ],
